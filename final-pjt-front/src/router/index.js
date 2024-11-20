f908385e989e48637d7f89c64538b90698372bab
조현준 <wns950404@naver.com>
2024-11-20 09:32:28 +0900

    :ambulance: 환율 계산기 프론트 누락으로 인한 재코딩 및 업로드; 기본 통화는 미국 달러로 설정

--- final-pjt-front/src/router/index.js
+++ final-pjt-front/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ExchangeRateView from '@/views/ExchangeRateView.vue'
+import CurrencyConverterView from '@/views/CurrencyConverterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/calc-exchange-rate',
-      name: 'exchangeRateCalculator',
-      component: ExchangeRateView,
+      path: '/currency-converter',
+      name: 'currencyConverter',
+      component: CurrencyConverterView,
     },
   ],
 })
--- final-pjt-front/src/stores/exchangeRate.js
+++ final-pjt-front/src/stores/exchangeRate.js
@@ -5,30 +5,19 @@ import axios from 'axios'
 export const useExchangeRateStore = defineStore(
   'exchangeRate',
   () => {
-    const API_KEY = '6N3X7TgXvz1cD1f9i6sxIHeuTKFH611J'
     const exchangeRates = ref([])
-
-    // create URL
-    const url = new URL(
-      'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
-    )
-    url.searchParams.append('authkey', API_KEY) // 인증키: OpenAPI 신청시 발급된 인증키
-    url.searchParams.append('data', 'AP01')     // 검색요청API타입: AP01 : 환율, AP02 : 대출금리, AP03 : 국제금리
-    // url.searchParams.append('searchdata', [값]) // 검색요청날짜: ex) 2015-01-01, 20150101, (DEFAULT)현재일
-    
+    const server_url = 'http://localhost:8000/exchange-rates/'
 
     const getExchangeRate = () => {
       axios
-        .get(url.toString())
+        .get(server_url)
         .then(res => {
-          exchangeRates.value = res.data
-          console.log(exchangeRates);
-          
+          exchangeRates.value = res.data.exchange_rates_data
         })
         .catch((err) => console.log(err))
     }
 
-    return { getExchangeRate }
+    return { exchangeRates, getExchangeRate }
   },
   { persist: true }
 )
